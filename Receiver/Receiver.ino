70aaed6790de324a4294b35ed33b8455486fb980
VirgileVP <virgilepornet@gmail.com>
2020-06-30 22:56:11 +0800

    feat(Receiver): Receiver code works

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,28 +1,41 @@
 #include <VirtualWire.h>
- 
-uint8_t buf[VW_MAX_MESSAGE_LEN];
-uint8_t buflen = VW_MAX_MESSAGE_LEN;
- 
+
+#define PIN_RED 3
+#define PIN_GREEN 5
+#define PIN_BLUE 11
+
+int pot_values[3] = {0, 0, 0};
+
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(9600);    
     vw_setup(2000);
+    pinMode(PIN_RED, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+    pinMode(PIN_BLUE, OUTPUT);
+    vw_set_rx_pin(7);
     vw_rx_start();
 }
- 
+
 void loop()
 {
-    if (vw_wait_rx_max(200))
+    byte receive_size = sizeof(pot_values);
+    vw_wait_rx();
+    if (vw_get_message((byte *) &pot_values, &receive_size))
     {
-        if (vw_get_message(buf, &buflen))
-        {
-            Serial.print("Receiving : ");
-            for (byte i = 0; i < buflen; i++)
-            {
-                Serial.print(buf[i]- '0');
-            }
-            Serial.println("");
-        }
-        Serial.print("\nDone\n");
+       pot_values[0] = map(pot_values[0], 0, 1024, 0, 255);
+       pot_values[1] = map(pot_values[1], 0, 1024, 0, 255);
+       pot_values[2] = map(pot_values[2], 0, 1024, 0, 255);
+       Serial.print("Receiving ...\n");
+       Serial.print("R : ");
+       Serial.print(pot_values[0]);
+       Serial.print(" G : ");
+       Serial.print(pot_values[1]);
+       Serial.print(" B : ");
+       Serial.print(pot_values[2]);
+       Serial.print("\n");
+       analogWrite(PIN_RED, pot_values[0]);
+       analogWrite(PIN_GREEN, pot_values[1]);
+       analogWrite(PIN_BLUE, pot_values[2]);
     }
 }
